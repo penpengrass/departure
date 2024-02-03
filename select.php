ab64e07c836ca01cb7c6add881e3c8878d3d589d
Hideaki-Iwahashi <hideaki981201@gmail.com>
2024-02-03 16:27:45 +0900

    select

--- select.php
+++ select.php
@@ -1,59 +1,59 @@
 <?php
+require('PHP/function1.php');
 session_start();
-$stationkey=0;
+$stationkey = 0;
 // ボタンがクリックされた場合
 if (isset($_POST['gifu'])) {
-    //$_SESSION['station'] = 1;
-    $test1='gifu';
+    $test1 = 'gifu';
     // 別のページにリダイレクト
-    header('Location: index7.php?station='.$test1);
+    header('Location: index7.php?station=' . $test1);
     exit();
 }
-if(isset($_POST['hamamatsu'])){
-    $_SESSION['station'] =2;
+exitfilejump(4,'hiroshima');
+if (isset($_POST['himeji'])) {
+    $test1 = 'himeji';
+    header('Location: index4.php?station=' . $test1);
+    exit();
+}
+if (isset($_POST['okayama'])) {
+    $test1 = 'okayama';
+    header('Location: index4.php?station=' . $test1);
+    exit();
+}
+if (isset($_POST['hamamatsu'])) {
+    $_SESSION['station'] = 2;
     //$files[0] = 'csv/JRC/gifu1.csv';
     // 別のページにリダイレクト
     header("Location: index7.php");
     exit();
 }
-if(isset($_POST['hiroshima'])){
-    $test1='hiroshima';
-    header('Location: index4.php?station='.$test1);
+if (isset($_POST['ogaki'])) {
+    $test1 = 'ogaki';
+    header('Location: index7.php?station=' . $test1);
     exit();
-} 
-if(isset($_POST['himeji'])){
-    $test1='himeji';
-    header('Location: index4.php?station='.$test1);
+}
+if (isset($_POST['numadu'])) {
+    $test1 = 'numadu';
+    header('Location: index7.php?station=' . $test1);
     exit();
-} 
-if(isset($_POST['okayama'])){
-    $test1='okayama';
-    header('Location: index4.php?station='.$test1);
+}
+if (isset($_POST['atami'])) {
+    $test1 = 'atami';
+    header('Location: index3.php?station=' . $test1);
     exit();
-} 
-if(isset($_POST['ogaki'])){
-    $test1='ogaki';
-    header('Location: index7.php?station='.$test1);
+}
+if (isset($_POST['maibara'])) {
+    $test1 = 'maibara';
+    header('Location: index4.php?station=' . $test1);
     exit();
-} 
-if(isset($_POST['numadu'])){
-    $test1='numadu';
-    header('Location: index7.php?station='.$test1);
+}
+if (isset($_POST['toyohashi'])) {
+    $test1 = 'toyohashi';
+    header('Location: index7.php?station=' . $test1);
     exit();
-} 
-if(isset($_POST['atami'])){
-    $test1='atami';
-    header('Location: index3.php?station='.$test1);
+}
+if (isset($_POST['staselect4'])) {
+    $test1 = $_POST['staselect4'];
+    header('Location: index4.php?station=okayama');
     exit();
-} 
-if(isset($_POST['maibara'])){
-    $test1='maibara';
-    header('Location: index4.php?station='.$test1);
-    exit();
-} 
-if(isset($_POST['toyohashi'])){
-    $test1='toyohashi';
-    header('Location: index7.php?station='.$test1);
-    exit();
-} 
-?>
+}
--- selection.php
+++ selection.php
@@ -4,20 +4,20 @@ function SelectCompany($fileName,$message){
 }
 //JR西日本在来線の駅選択
 function JRWStaSele($Select){
-    echo '<form action="index4.php" method="POST" id="selectstation">
-                <select name="stasele">
-                    <option value="csv/JRW/JRS1.csv">北新地駅</option>
-                    <option value="csv/JRW/maibara1.csv">米原駅</option>
-                    <option value="csv/JRW/tennoji1.csv">天王寺駅</option>
-                    <option value="csv/JRW_S/himeji_S1.csv">姫路駅</option>
-                    <option value="csv/JRW/SanyoArea/okayama_sanyo1.csv">岡山駅</option>
-                    <option value="csv/JRW/SanyoArea/itozaki1.csv">糸崎駅</option>
-                    <option value="csv/JRW/SanyoArea/mihara1.csv">三原駅</option>
-                    <option value="csv/JRW/SanyoArea/hiroshima1.csv">広島駅</option>
-                    <option value="csv/JRW/niimi1.csv">新見駅</option>
-                    <option value="csv/JRW/SanyoArea/iwakuni1.csv">岩国駅</option>
-                    <option value="csv/JRW/SanyoArea/tokuyama1.csv">徳山駅</option>
-                    <option value="csv/JRW/SanyoArea/shimonoseki1.csv">下関駅</option>
+    echo '<form action="PHP/files4.php" method="POST" id="selectstation">
+                <select name="staselect4">
+                    <option value="kitashinti">北新地駅</option>
+                    <option value="maibara">米原駅</option>
+                    <option value="tennouji">天王寺駅</option>
+                    <option value="himeji">姫路駅</option>
+                    <option value="okayama">岡山駅</option>
+                    <option value="itozaki">糸崎駅</option>
+                    <option value="mihara">三原駅</option>
+                    <option value="hiroshima">広島駅</option>
+                    <option value="niimi">新見駅</option>
+                    <option value="iwakuni">岩国駅</option>
+                    <option value="tokuyama">徳山駅</option>
+                    <option value="shimonoseki">下関駅</option>
                 </select>
                 <button type="submit" name="submit">'.$Select.'</button>
             </form>';
