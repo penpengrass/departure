e22a7e4ef781a4d9610692cb62c396bfe519da37
penpengrass <hideaki981201@gmail.com>
2026-03-16 18:47:45 +0900

    旧JSファイル削除

--- js/Time.js
+++
@@ -1,7 +0,0 @@
-const now = /* @__PURE__ */ new Date();
-const hour = now.getHours();
-const min = now.getMinutes();
-const sec = now.getSeconds();
-window.L_hour = hour;
-window.L_min = min;
-`${hour}:${String(min).padStart(2, "0")}:${String(sec).padStart(2, "0")}`;
--- js/Timer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  window.setInterval(
-    function() {
-      var dat = /* @__PURE__ */ new Date();
-      document.getElementById("TTime").textContent = dat.toLocaleTimeString();
-    },
-    100
-  );
-}, false);
